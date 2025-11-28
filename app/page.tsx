b8feb3c41305a0f8e5d2e59e46d6974534cee251
cwh <cwh1229@naver.com>
2025-11-28 12:46:00 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
+              <Link href={"/"}>The List</Link>
 
 
             <div className="flex gap-3">
@@ -31,9 +31,6 @@ export default function Home() {
               </Button>
             </div>
 
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
             </div>
 
             {!hasEnvVars ? (
